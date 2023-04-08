b66ed81e278d86ba06b19f72c02b5e69f165f859
Huy <baohuyvongoc1@gmail.com>
2023-04-08 16:15:35 +0700

    add modal, checkbox

---
+++ js/Admin/main.js
@@ -0,0 +1,7 @@
+function toggle(source) {
+    checkboxes = document.getElementsByName('checkboxcategory');
+    for(var i=0, n=checkboxes.length;i<n;i++) {
+      checkboxes[i].checked = source.checked;
+    }
+  }
+  
